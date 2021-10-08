96bac97dce43f702ee0ac72cc0514a67d844305c
Filippo Pisello <filippo.pisello@live.it>
2021-10-08 15:40:40 +0200

    fix: correct import and connector call

--- database/db_connect.py
+++ database/db_connect.py
@@ -1,6 +1,7 @@
 """Use credentials in db_credentials to access the database"""
 import mysql.connector
-from db_credentials import CD
+
+from database.db_credentials import CD
 
 
 db = mysql.connector.connect(host=CD.host, user=CD.username, passwd=CD.password)
@@ -13,4 +14,4 @@ DB: mysql.connector.CMySQLConnection = mysql.connector.connect(
     passwd=CD.password,
     database="books_database",
 )
-CURSOR: mysql.connector.cursor_cext.CMySQLCursor = db.cursor()
+CURSOR: mysql.connector.cursor_cext.CMySQLCursor = DB.cursor()
