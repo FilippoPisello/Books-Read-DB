3a22a65eb0c0e1c5f89a907dc3372fea32968734
Filippo Pisello <filippo.pisello@live.it>
2021-10-11 09:53:42 +0200

    feat: retrive last added book

--- database/db_actions.py
+++ database/db_actions.py
@@ -21,6 +21,36 @@ def add_book_to_db(
     DB.commit()
 
 
+def get_last_book(fields: Union[str, list[str]] = "All") -> tuple:
+    """Retrive the last book added to the database"""
+    fields = parse_field_input(fields)
+    last_id = get_last_id()
+    query = f"SELECT {fields} FROM Book WHERE book_pk = %s"
+    CURSOR.execute(query, (last_id,))
+    return CURSOR.fetchone()
+
+
+def get_last_id() -> str:
+    last_id = CURSOR.lastrowid
+    if last_id != 0:
+        return last_id
+    CURSOR.execute("SELECT MAX(book_pk) FROM Book")
+    return CURSOR.fetchone()[0]
+
+
+def parse_field_input(field_input: Union[str, list[str]]) -> str:
+    """Transform an indication of field into str to be passed to query"""
+    if field_input == "All":
+        return "*"
+    if isinstance(field_input, list):
+        return " ".join(field_input)
+    if isinstance(field_input, str):
+        return field_input
+    raise TypeError(
+        f"Field input should be str or list, {type(field_input)} was provided."
+    )
+
+
 def remove_book_given_id(book_id: int) -> None:
     """Remove a book from the DB given its ID"""
     # To avoid wrong unwanted injections
--- tests/test_dbactions.py
+++ tests/test_dbactions.py
@@ -13,6 +13,30 @@ class TestDBActions(unittest.TestCase):
         )
         DB.commit()
 
+    def test_get_last_book(self):
+        """Test whether last book added is correctly retrived"""
+        # Add book first
+        content = ("Test", "Test xy", "Test", 120, "Test", False, "Test, Test")
+        addition_query = """
+        INSERT INTO Book (title, author_name, author_surname, pages, genre, owned, tags)
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """
+        CURSOR.execute(addition_query, content)
+        DB.commit()
+
+        # All fields
+        added_book = act.get_last_book(fields="All")
+        self.assertEqual(added_book[0], "Test")
+        self.assertEqual(added_book[1], "Test xy")
+        self.assertEqual(added_book[5], False)
+        self.assertEqual(added_book[6], "Test, Test")
+
+        # Single field
+        added_book = act.get_last_book(fields="title")
+        self.assertEqual(added_book[0], "Test")
+        added_book = act.get_last_book(fields="author_name")
+        self.assertEqual(added_book[0], "Test xy")
+
     def test_addbook(self):
         """Test that a book is correctly added to the database"""
         act.add_book_to_db("Test", "Test", "Test", 120, "Test", False, "Test, Test")
