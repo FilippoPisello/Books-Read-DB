5f8d3683fdf78d566671e6d58284fc8e2d773e8c
Filippo Pisello <filippo.pisello@live.it>
2021-10-16 19:20:52 +0200

    feat: add fields proper of read

--- interface/gui.py
+++ interface/gui.py
@@ -4,11 +4,23 @@ from gooey import Gooey, GooeyParser
 @Gooey(
     program_name="Book Database",
     program_description="Never lose track of the books you read!",
+    default_size=(600, 800),
 )
 def gui_main() -> dict[str, str]:
+    """Render the GUI for the user to provide inputs"""
     parser = GooeyParser()
-    book_group = parser.add_argument_group("Book info")
 
+    add_book_fields(parser)
+    add_read_fields(parser)
+
+    args = vars(parser.parse_args())
+    print(args)
+    return args
+
+
+def add_book_fields(parser: GooeyParser) -> None:
+    """Add the book related fields to the GUI parser"""
+    book_group = parser.add_argument_group("Book info")
     book_group.add_argument(
         "Book title",
         gooey_options={"full_width": True},
@@ -29,6 +41,7 @@ def gui_main() -> dict[str, str]:
         "--Pages",
         help="Add the number of pages",
         widget="IntegerField",
+        gooey_options={"min": 1, "max": 10000},
     )
     book_group.add_argument(
         "Genre",
@@ -40,10 +53,37 @@ def gui_main() -> dict[str, str]:
         help="Click if you own the book",
         default=False,
     )
+    book_group.add_argument(
+        "--Tags",
+        help="Add some comma-separated tags",
+    )
 
-    args = vars(parser.parse_args())
-    print(args)
-    return args
+
+def add_read_fields(parser: GooeyParser) -> None:
+    """Add the read related fields to the GUI parser"""
+    read_group = parser.add_argument_group("Reading info")
+    read_group.add_argument(
+        "Starting date",
+        help="Pick the date when you started reading the book",
+        widget="DateChooser",
+    )
+    read_group.add_argument(
+        "Ending date",
+        help="Pick the date when you finished the book",
+        widget="DateChooser",
+    )
+    read_group.add_argument(
+        "--Score",
+        help="Choose a score from 0 to 10",
+        gooey_options={"min": 0, "max": 10},
+        widget="Slider",
+        default=None,
+    )
+    read_group.add_argument(
+        "--Comment",
+        help="Add a comment on the book",
+        default=None,
+    )
 
 
 if __name__ == "__main__":
